dfbc6054c8024f197ca453a9ab0672bd0894b540
Mercurio1509 <moisesleiva01@gmail.com>
2023-09-09 22:13:18 -0600

    Update Script_AM_DSB_SC.m

--- Script_AM_DSB_SC.m
+++ Script_AM_DSB_SC.m
@@ -1,4 +1,4 @@
-syms t
+%syms t2;
 
 N=1024;
 Fs=2*2500;
@@ -7,9 +7,9 @@ t=(0: N-1)*Ts;
 
 %Señal Moduladora 1 
 M1=0.5;
-F_mod1=60;
-A_mod1=1;
-S_mod1=A_mod1*cos(2*pi*F_mod1*t)+1;
+F_mod1=10;
+A_mod1=5;
+S_mod1=A_mod1*cos(2*pi*F_mod1*t);
 figure(1)
 subplot(2,1,1); 
 plot(t, S_mod1)
@@ -18,7 +18,7 @@ title("Señal Moduladora 1");
 %Señal Portadora 1
 %A_port=A_mod/M;
 A_port1=1;
-F_port1=1000;
+F_port1=500;
 S_port1=A_port1*cos(2*pi*F_port1*t);
 figure(1)
 subplot(2,1,2); 
@@ -48,8 +48,8 @@ ylabel("|P1(f)|")
 %Señal Moduladora 2 
 M2=0.5;
 F_mod2=100;
-A_mod2=1;
-S_mod2=A_mod2*cos(2*pi*F_mod2*t)+1;
+A_mod2=10;
+S_mod2=A_mod2*cos(2*pi*F_mod2*t);
 figure(3)
 subplot(2,1,1); 
 plot(t, S_mod2)
@@ -72,7 +72,7 @@ plot(t, AM2)
 title("Señal Modulada 2")
 
 %MULTIPLEXACIÓN DE SEÑALES
-Suma=AM1+AM2+7*cos(2*pi*100*t);
+Suma=AM1+AM2+5*cos(2*pi*200*t);
 figure(5)
 plot(t,Suma)
 title("Señal Sumada AM")
@@ -104,96 +104,61 @@ title("Single-Sided Amplitude Spectrum of Suma(t)")
 xlabel("f (Hz)")
 ylabel("|P1(f)|")
 
-%Filtrado de señal media
-Wpass=[800 1500];
-Filtrado=bandpass(Suma,Wpass,Fs);
-Y=fft(Filtrado);
-P2 = abs(Y/N);
-P1 = P2(1:N/2+1);
-P1(2:end-1) = 2*P1(2:end-1);
-f = Fs*(0:(N/2))/N;
-figure(8)
-plot(f,P1) 
-title("Filtrado despues de paso banda")
-xlabel("f (Hz)")
-ylabel("|P1(f)|")
-figure(9)
 
-plot(t,Filtrado);
-title("Señal media filtrada en el tiempo")
 
-%Filtrado de señal baja
-Filtrado=lowpass(Suma,750,Fs);
-Y=fft(Filtrado);
-P2 = abs(Y/N);
-P1 = P2(1:N/2+1);
-P1(2:end-1) = 2*P1(2:end-1);
-f = Fs*(0:(N/2))/N;
-figure(10)
-plot(f,P1) 
-title("Filtrado despues de paso bajo")
-xlabel("f (Hz)")
-ylabel("|P1(f)|")
-figure(11)
-
-plot(t,Filtrado);
-title("Señal baja filtrada en el tiempo")
-
-%Filtrado de señal alta
-Filtrado=highpass(Suma,1500,Fs);
-Y=fft(Filtrado);
-P2 = abs(Y/N);
-P1 = P2(1:N/2+1);
-P1(2:end-1) = 2*P1(2:end-1);
-f = Fs*(0:(N/2))/N;
-figure(10)
-plot(f,P1) 
-title("Filtrado despues de paso alto")
-xlabel("f (Hz)")
-ylabel("|P1(f)|")
-figure(11)
-
-plot(t,Filtrado);
-title("Señal alta filtrada en el tiempo")
 %--------------Modulacion FM----------------------
 
-% m = Suma; %mensaje a modular
-% 
-% fDev = 50;
-% 
-% fc_fm = 10000;
-% Fs=2*fc_fm;
-% mi=2;
-% %t = (0:1/Fs:0.01);
-% 
-% s_fm = fmmod(m,fc_fm,Fs,fDev);
-% 
-% figure(8)
-% plot(t,m,'c',t,s_fm,'b--')
-% xlabel('Time (s)')
-% ylabel('Amplitude')
-% legend('Original Signal','Modulated Signal')
-% 
-% 
-% fm_demod = fmdemod(s_fm,fc_fm,Fs,fDev);
-% 
-% 
-% 
-% 
-% %Espectro Señal demodulada FM
-% Y=fft(fm_demod);
-% P2 = abs(Y/N);
-% P1 = P2(1:N/2+1);
-% P1(2:end-1) = 2*P1(2:end-1);
-% f = Fs*(0:(N/2))/N;
-% figure(10)
-% plot(f,P1) 
-% title("Single-Sided Amplitude Spectrum of señal FM demodulada")
-% xlabel("f (Hz)")
-% ylabel("|P1(f)|")
-% 
+m = Suma; %mensaje a modular
+
+fDev = 10;
+ 
+fc_fm = 2500;
+Fs_fm=2*fc_fm;
+Ts_fm=1/Fs_fm;
+mi=2;
+t2 = (0: N-1)*Ts_fm;
+ 
+s_fm = fmmod(m,fc_fm,Fs_fm,fDev);
+figure(8)
+plot(t, s_fm)
+%xlabel('Time (s)')
+%ylabel('Amplitude')
+legend('Modulated Signal')
+
+
+fm_demod = fmdemod(s_fm,fc_fm,Fs_fm,fDev);
+
+%Espectro SeÃ±al demodulada FM
+Y=fft(s_fm);
+P2 = abs(Y/N);
+P1 = P2(1:N/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+f = Fs_fm*(0:(N/2))/N;
+figure(10)
+plot(f,P1) 
+title("Single-Sided Amplitude Spectrum of señal FM modulada")
+xlabel("f (Hz)")
+ylabel("|P1(f)|")
+ 
+ 
+ 
+ 
+%Espectro SeÃ±al demodulada FM
+Y=fft(fm_demod);
+P2 = abs(Y/N);
+P1 = P2(1:N/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+f = Fs_fm*(0:(N/2))/N;
+figure(11)
+plot(f,P1) 
+title("Single-Sided Amplitude Spectrum of señal FM demodulada")
+xlabel("f (Hz)")
+ylabel("|P1(f)|")
+ 
 % figure(11)
 % plot(t,m,'c',t,fm_demod,'b--')
 % xlabel('Time (s)')
 % ylabel('Amplitude')
 % legend('Moduladora','Demodulacion FM')
+
+
